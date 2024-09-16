bd015c7a917b7d3ce77d4305d5115e5cc829de36
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2024-09-16 21:40:26 +0100

    adding user name & avatar

---
+++ database/migrations/2024_09_16_203621_add_columns_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username', 1024);
+            $table->string('avatar_path', 1024)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username', 1024);
+            $table->dropColumn('avatar_path', 1024);
+        });
+    }
+};
